aab60ebd468db0a875e85944bbc5631565b48856
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-09-30 22:39:26 +0300

    emulated the board as an array

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 const positions = document.querySelectorAll(".position");
 let player_cpu = false;
+let game_state = true;
+const board = ["e","e","e","e","e","e","e","e","e"];
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 const img3 = document.getElementById("img3");
@@ -20,25 +22,33 @@ positions[0].addEventListener("click", () => {
             //img1.classList.add("animate_red");
             img1.src = "red.png";
             player_cpu = false;
+            board[0]="o";
         }
         else {
             img1.src = "yellow.png";
             player_cpu = true;
+            board[0]="x";
         }
     }
-    
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 positions[1].addEventListener("click", () => {
     if (img2.src == "http://127.0.0.1:5500/Assignment3/Web_Assignment3/empty.png") {
         if (player_cpu) {
             img2.src = "red.png";
             player_cpu = false;
+            board[1]="o";
+
+
+            
         }
         else {
             img2.src = "yellow.png";
             player_cpu = true;
+            board[1]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -47,12 +57,15 @@ positions[2].addEventListener("click", () => {
         if (player_cpu) {
             img3.src = "red.png";
             player_cpu = false;
+            board[2]="o";
         }
         else {
             img3.src = "yellow.png";
             player_cpu = true;
+            board[2]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -61,13 +74,15 @@ positions[3].addEventListener("click", () => {
         if (player_cpu) {
             img4.src = "red.png";
             player_cpu = false;
+            board[3]="o";
         }
         else {
             img4.src = "yellow.png";
             player_cpu = true;
+            board[3]="x";
         }
     }
-
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -76,12 +91,15 @@ positions[4].addEventListener("click", () => {
         if (player_cpu) {
             img5.src = "red.png";
             player_cpu = false;
+            board[4]="o";
         }
         else {
             img5.src = "yellow.png";
             player_cpu = true;
+            board[4]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -90,12 +108,15 @@ positions[5].addEventListener("click", () => {
         if (player_cpu) {
             img6.src = "red.png";
             player_cpu = false;
+            board[5]="o";
         }
         else {
             img6.src = "yellow.png";
             player_cpu = true;
+            board[5]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -104,12 +125,15 @@ positions[6].addEventListener("click", () => {
         if (player_cpu) {
             img7.src = "red.png";
             player_cpu = false;
+            board[6]="o";
         }
         else {
             img7.src = "yellow.png";
             player_cpu = true;
+            board[6]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -118,12 +142,15 @@ positions[7].addEventListener("click", () => {
         if (player_cpu) {
             img8.src = "red.png";
             player_cpu = false;
+            board[7]="o";
         }
         else {
             img8.src = "yellow.png";
             player_cpu = true;
+            board[7]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
 
@@ -132,11 +159,14 @@ positions[8].addEventListener("click", () => {
         if (player_cpu) {
             img9.src = "red.png";
             player_cpu = false;
+            board[8]="o";
         }
         else {
             img9.src = "yellow.png";
             player_cpu = true;
+            board[8]="x";
         }
     }
+    console.log(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
 });
 
